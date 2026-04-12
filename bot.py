bf2b3fa97e24d7b310abae7305e9a62380076f89
REDIS <radattt051@gmail.com>
2026-04-12 19:12:12 +0300

    fix structure

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "ТВОЙ_ТОКЕН"
+TOKEN = "8738925055:AAFb_S_H1VAyfirpW3n6WW3INXV2TYdbR_o"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [InlineKeyboardButton(
             "🐟 Играть",
-            web_app=WebAppInfo(url="https://ТВОЯ-ССЫЛКА")
+            web_app=WebAppInfo(url="https://fish-game-delta.vercel.app/")
         )]
     ]
 
